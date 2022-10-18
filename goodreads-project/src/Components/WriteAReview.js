910badcd457d389f5871b2cf2e519ce7b8c56826
Hristo Mangarudov <wodbv1@gmail.com>
2022-10-18 09:40:57 +0300

    added post review form

--- goodreads-project/src/Components/WriteAReview.js
+++ goodreads-project/src/Components/WriteAReview.js
@@ -1,7 +1,13 @@
 import BookCard from "./BookCard";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 function WriteAReview(props) {
   return (
     <div>
+        <div className="headers-container-titles">
+            <h4>A Sign of Affection</h4>
+            <h5>{'>'}Review</h5>
+        </div>
       <div className="cards-wrapper">
         <div className="reviewImgContainer">
           <img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1470082995l/29056083._SY475_.jpg" />
@@ -14,7 +20,16 @@ function WriteAReview(props) {
           </p>
         </div>
       </div>
-      <div>
+      <div className="review-container">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>What do you think?</Form.Label>
+            <Form.Control as="textarea" type="text" placeholder="Write your thoughts here" style={{height: "500px"}} />
+          </Form.Group>
+          <Button variant="secondary" type="submit">
+            Post
+          </Button>
+        </Form>
       </div>
     </div>
   );
