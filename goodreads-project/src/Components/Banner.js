152bb2a2400f0ca950ec98b876e0f8327d6de19a
Hristo Mangarudov <wodbv1@gmail.com>
2022-10-20 21:12:25 +0300

    added a banner

---
+++ goodreads-project/src/Components/Banner.js
@@ -0,0 +1,12 @@
+import "./banner.scss"
+function BannerComponent(){
+return(
+    <div className="banner-wrapper">
+        <div className="banner-container">
+            <h1>Welcome to our site!</h1>
+        </div>
+    </div>
+
+)
+}
+export default BannerComponent
--- goodreads-project/src/pages/myBooksPage/MyBooksPage.js
+++ goodreads-project/src/pages/myBooksPage/MyBooksPage.js
@@ -2,7 +2,7 @@ import MyBooksTable from "../../Components/MyBooksTable";
 function MyBooksPage(){
 return(
     <div className="cards-wrapper">
-    <div className="cards-container">
+    <div className="cards-container  general-container">
       <MyBooksTable />
     </div>
   </div>
