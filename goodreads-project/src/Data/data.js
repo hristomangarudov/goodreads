0bbac3d959a0b56a39e6f39fc0189400a06be28a
AleksandarBuzov <aleksandarbuzov00@gmail.com>
2022-10-30 03:03:29 +0300

    JSON file with comments

---
+++ goodreads-project/src/Data/data.js
@@ -0,0 +1,314 @@
+const dataReviews = [
+  {
+    "id": 1,
+    "profilePicture": "https://images.gr-assets.com/users/1597876967p2/1343864.jpg",
+    "username": "Sara the Librarian",
+    "rate": "&#9733;&#9733;",
+    "review": `Even if I'd absolutely hated this book I'd have to credit author Annelise Ryan with creating perhaps the most unusual amateur sleuth I've yet to come across. Morgan Carter, who owns a creepy little book and curio shop called\"Odds and Ends\" in a tourist town in Wisconsin (TIL Wisconsin has tourists towns) also has a side hustle going as...a monster hunter...dun dun DUN!`,
+  },
+  {
+    "id": 2,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1666801636p2/54564996.jpg",
+    "username": "Robyn",
+    "rate": "4",
+    "review": `OK, this is one crazy cozy mystery! I actually loved the twisted plot and the education about cryptozoologists. The characters were relatable, entertaining, and appropriately fleshed out. There is an air of humor and just a touch of romance to brighten the day. It was a super fun read.`,
+  },
+  {
+    "id": 3,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1662575239p2/5856676.jpg",
+    "username": "Maggie Carr",
+    "rate": "3",
+    "review": `Simply put it felt like a cozy mystery that softened the death and fearful moments with a dose of smart humor, or quick wit releasing the tension as it was still building. Not a bad read but I think I was looking to be a little more scared.`,
+  },
+  {
+    "id": 4,
+    "profilePicture":
+      "https://s.gr-assets.com/assets/nophoto/user/f_50x66-6a03a5c12233c941481992b82eea8d23.png",
+    "username": "Cathy",
+    "rate": "3",
+    "review": `This was just okay. Not nearly as good as dark and shallow lies. Nevertheless, it was a quick read and was somewhat entertaining.`,
+  },
+  {
+    "id": 5,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1392688703p2/1801101.jpg",
+    "username": "Kat",
+    "rate": "3",
+    "review": `I was hoping the vibe of the book would be more along the lines of the playful episodes of Supernatural and the X-Files. But the author doesn’t make references to the sci-fi pop culture world—it is more of a straightforward look at cryptid theories.`,
+  },
+  {
+    "id": 6,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1648932586p2/44348478.jpg",
+    "username": "Cheyenne",
+    "rate": "3",
+    "review": `This was basically an adult version of Scooby-Doo and I really enjoyed it! It was very detailed and had a really intricate storyline, but it kept me listening. And with the touch of romance here I’m looking forward to the next book, especially if we get to learn more about Morgan’s parents!`,
+  },
+  {
+    "id": 7,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1656046803p2/2190064.jpg",
+    "username": "Angela Staudt ",
+    "rate": "2",
+    "review": `Here comes a review that I just really don't want to write. Dark and Shallow Lies was one of my favorite books of 2021. It was phenomenal and I devoured that book so fast. I had very high hopes for this book, but sadly it just was not for me.`,
+  },
+  {
+    "id": 8,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1610570934p2/8579437.jpg",
+    "username": "Rylan",
+    "rate": "4",
+    "review": `The characters were amazing and I loved them!! I loved her boy best friend the most. I also didn’t know that there was going to be any romance sub plot in this book but it made it even better and kept me very interested. Cole and Avril are everything.`,
+  },
+  {
+    "id": 9,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1662175965p2/3553120.jpg",
+    "username": "Maddie",
+    "rate": "3",
+    "review": `This is an interesting book- there's mystery, romance, and hints of supernatural- though that never really gets explored beyond an old ghost story, I do wish the author would have utilized that more.
+      I'm not a reader of YA but the story did interest me and pull me in. It moved slower than I would have preferred, and I could have done without so much romance. The characters were interesting but I would have liked to see some hint of their futures.`,
+  },
+  {
+    "id": 10,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1664457064p2/3660666.jpg",
+    "username": "Bethany",
+    "rate": "4",
+    "review": `Dark, sensual, and dangerous. House of Hunger is a queer fantasy horror novel drawing on the history of the Countess of Bathory (known for bathing in the blood of virgins to stay young). In this world pretty young woman can become bloodmaids to wealthy patrons. The consumption of their blood can cure illness and is considered a delicacy for the elite.`,
+  },
+  {
+    "id": 11,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1657384030p2/40515199.jpg",
+    "username": "Brittany Smith",
+    "rate": "5",
+    "review": `Thank you to the publisher and Edelweiss for the advanced readers copy in exchange for an honest review.
+      As soon as I saw the premise of this shared on Twitter, I knew deep in my bones that I desperately needed to read this, it sounded so finely attuned to my specific tastes, like it was written for me.`,
+  },
+  {
+    "id": 12,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1641074093p2/3030788.jpg",
+    "username": "Melanie",
+    "rate": "2",
+    "review": `one chapter in and... reading new words and characters and atmosphere from my favorite author? the best kind of magic that always feels like coming home ;_____; (but this time with vampires!! and vampire hunters!!)`,
+  },
+  {
+    "id": 13,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1597282744p2/5841469.jpg",
+    "username": "Andy",
+    "rate": "4",
+    "review": `This was the polyamorous queer vampire book of my dreams!! I loved this book so much. And I read it with my eyes 😌. If you know me, you know that audiobooks and screen listening to ebooks is the main way I read books. But I decided to read this one with my eyes. The plan was to spread this chunker out over a month. `,
+  },
+  {
+    "id": 14,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1615962100p2/42225826.jpg",
+    "username": "Kit",
+    "rate": "3",
+    "review": `I didn't really feel the romantic feelings between Remy and the other two. It seemed to be mostly based on physical attraction and the little bit they knew of each other through fighting together or against each other. Which works for initial feelings, but they jumped straight into being all-in and wanting to commit. To be fair, it's not hard to understand why Remy, or anyone, would want Zidan. He was smart, a skilled fighter, poised, kind, supportive, sexy, and a broody vampire, to boot. But even that isn't quite enough for me to actually feel the romantic chemistry between two characters.`,
+  },
+  {
+    "id": 15,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1587657715p2/8338960.jpg",
+    "username": "Darla",
+    "rate": "4",
+    "review": `This new series could be the "Roots" for the 21st century. I have enjoyed many of Kwame Alexander's other titles, especially the sports-theme books for middle grade. As a Gen Xer, I am not the intended audience for this book. If I had not already experienced "Roots" in print and on TV as a mini-series, this might have gotten 5 stars. I read this in print, but would expect this title to be especially meaningful on audio.`,
+  },
+  {
+    "id": 16,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1622412049p2/6021548.jpg",
+    "username": "Shannon",
+    "rate": "4",
+    "review": `A powerfully, moving middle grade story about a young African boy who gets stolen and sold into slavery. Perfect for fans of the show Roots or Lawrence Hill's Book of negroes and great on audio read by the author! I'm excited that this is the first in a trilogy and can't wait to see where the series goes next!! Much thanks to Librofm for an ALC of this great new book!`,
+  },
+  {
+    "id": 17,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1661130742p2/781011.jpg",
+    "username": "Melodi",
+    "rate": "5",
+    "review": `I picked this up without refreshing myself on the synopsis and I recommend you do that too! I read this in one day and now I'm sitting with this book and these words in a heavy, yet beautiful place in my heart.`,
+  },
+  {
+    "id": 18,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1652886322p2/90109412.jpg",
+    "username": "Kristen Slora",
+    "rate": "3",
+    "review": `This fell short for me. I really loved her other two books, but this one didn’t offer anything new or really profound. The transitions were, as I read in another review, choppy. It really feels like she is just riding on her aesthetic, a lot of her poems lack real creativity.`,
+  },
+  {
+    "id": 19,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1564671639p2/50092845.jpg",
+    "username": "Alexis",
+    "rate": "5",
+    "review": `This book is like looking into a mirror if you suffer from depression and anxiety. The latter is an old friend of mine but the former? I had no idea she was there in the background all this time, following me wherever I went and waiting for me to turn around and acknowledge her.`,
+  },
+  {
+    "id": 20,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1651264034p2/54790180.jpg",
+    "username": "Deborah Obida",
+    "rate": "5",
+    "review": `Amazing, I love every part of it. This is my second poetry by Rupi and this is as good as the other one I read. The writing is great and very relatable, I love that about this book. Below are some quotes from the book.`,
+  },
+  {
+    "id": 21,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1459773375p2/2970379.jpg",
+    "username": "Manybooks",
+    "rate": "2",
+    "review": `Please note that my rather negative reading reaction to Lisa Fipps’ 2021 novel in verse Starfish seems to be rather the minority, but sorry, in particular on an emotional level, I have not really enjoyed the presented narrative, I have found Starfish extremely painful and majorly uncomfortable.`,
+  },
+  {
+    "id": 22,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1471222501p2/14435850.jpg",
+    "username": "Ashlie Elizabeth",
+    "rate": "5",
+    "review": `I read this entire galley in a single sitting and finished with tears pouring down my cheeks. This book is blunt and beautiful. There are literally a dozen people I want to buy copies for. I love the concept that this book was originally going to be for a YA audience until it was realized that a middle grade book would catch young people as their identities were developing and they needed the message the most.`,
+  },
+  {
+    "id": 23,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1295167791p2/800291.jpg",
+    "username": "Andrew",
+    "rate": "5",
+    "review": `Books like this I find to hard to comment on - even if they are more my personal views rather than professional in-depth reviews. You see this is one of those books with beautifully simplistic artwork with poignant and thoughtful comments and you would be right that book shops seem to have so many different variations of this they almost have created their own genre.`,
+  },
+  {
+    "id": 24,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1539445641p2/46662575.jpg",
+    "username": "Lucy",
+    "rate": "5",
+    "review": `This book already made me tear up in the bookshop - I thought it was so cute 🥺. This book really made me happy, and it was exactly what I needed in this moment`,
+  },
+  {
+    "id": 25,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1654282684p2/151902338.jpg",
+    "username": "Roppina",
+    "rate": "4",
+    "review": `This book is beautiful and adorable pictures. I recommend this book. Big Panda and Little Dragon is a book about friendship and wisdom. This is an amazing book to share with family and friends. We all have flaws, but we are all perfect.`,
+  },
+  {
+    "id": 26,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1465042652p2/56446521.jpg",
+    "username": "Anne",
+    "rate": "4",
+    "review": `This was just really cute and heartwarming and reading it just felt like a big hug - I will definitely browse through this on some low days in the future. I've been looking more into mindfulness and meditation and I think this book would be a good one for anyone who wants to look more into that just because it really does talk about a lot of the similar themes.`,
+  },
+  {
+    "id": 27,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1568145522p2/95805301.jpg",
+    "username": "Veronica Rychtarczyk",
+    "rate": "4",
+    "review": `This book is filled with cute illustrations, humor, and wisdom. A light, uplifting read about two unlikely friends who weather the seasons together.`,
+  },
+  {
+    "id": 28,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1483904903p2/50406058.jpg",
+    "username": "Lindy",
+    "rate": "3",
+    "review": `I felt like a lot of the pages or little quotes were getting repetitive, without fully expanding on an idea or thought entirely, just reusing the same lines over and over. There were also a few typos in my copy. I did find this book to have some poems that were quite profound and underlined quite a few lines and tagged some pages to read again later. Overall okay. I think I liked her previous book more.`,
+  },
+  {
+    "id": 29,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1660886035p2/80811491.jpg",
+    "username": "Mia",
+    "rate": "3",
+    "review": `It is quite a quote-filled book that I found it hard to finish in one setting. It is like a meal, it has to be savoured slowly because it makes you think and enjoy the words. For me, it definitely brought me back to some instances of my life where I think I could have done better or made a better decision. It also helped me to reflect about what can I do to make my life better.`,
+  },
+  {
+    "id": 30,
+    "profilePicture":
+      "https://images.gr-assets.com/users/1609852266p2/122208914.jpg",
+    "username": "Cristine",
+    "rate": "4",
+    "review": `A Gentle Reminder is everything we ever need. If at all you feel like you are not enough, not doing enough, not happy enough or be anything enough just pick up this book. They say time heals but you know words heal anytime.`,
+  },
+  {
+    "id": 31,
+    "profilePicture":"https://images.gr-assets.com/users/1477433724p2/2431873.jpg",
+    "username": "Emily",
+    "rate": "2",
+    "review": `I really enjoyed Fable, so I was very excited to get this book on Netgalley. This is Adrienne Young's first adult book, but the core of the story is very YA, and the book seems confused about what it wants to be. Is it a mystery? Magical realism? It doesn't do either well, so I ended up disappointed. You may like this book if you enjoy atmospheric reads. The problem is that there's not much under the surface.`,
+  },
+  {
+    "id": 32,
+    "profilePicture":"https://images.gr-assets.com/users/1665407303p2/4987440.jpg",
+    "username": "Stephan",
+    "rate": "3",
+    "review": `I wish something stood out for me but in all honesty, everything about this book was mediocre. Also a missed opportunity to play up the witchy aspect of the book, it read more like an extremely dull mystery/thriller instead. Deep tortured sigh.`,
+  },
+  {
+    "id": 33,
+    "profilePicture":"https://images.gr-assets.com/users/1395274464p2/28831998.jpg",
+    "username": "Chelsea",
+    "rate": "2",
+    "review": `I don't think this story needed to be told from more than one perspective. The characters didn't have enough personality to be able to tell them apart. I didn't like the writing and the story was boring, there wasn't really any magic until almost the end of the book. Even though this is supposed to be adult, it felt very YA.`,
+  },
+  {
+    "id": 34,
+    "profilePicture":"https://images.gr-assets.com/users/1643054528p2/135171711.jpg",
+    "username": "Aleksandra",
+    "rate": "3",
+    "review": `This was an enchanting and intriguing fantasy about love, revenge, and power. Kingdom of the Feared follows Emilia as she deals with issues of her past that have come to the surface while also trying to solve a murder. At the same time Emilia and Wrath must stop a war that is brewing between witches, demons and shapeshifters.`,
+  },
+  {
+    "id": 35,
+    "profilePicture":"https://images.gr-assets.com/users/1664266204p2/51289885.jpg",
+    "username": "Sylvie",
+    "rate": "2",
+    "review": `I'll begin by saying that this series started out strong. The first book was excellent. The second was less so, but I thought it was just a bridge type of sequel in a trilogy, so I didn't mind that it was overly long and that nothing important seemed to happen. I thought the second book was just setting up an epic ending, and I was excited for it.`,
+  },
+  {
+    "id": 36,
+    "profilePicture":"https://images.gr-assets.com/users/1335060225p2/8951745.jpg",
+    "username": "Johnny",
+    "rate": "5",
+    "review": `A story in poetry exploring the intersections of queerness and Asian American identity, this book was everything I could have hoped for and more from Chen Chen’s sophomore work. What beauty and erratic honesty built through Chen’s life experiences and now shared to the reader.`,
+  },
+  {
+    "id": 37,
+    "profilePicture":"https://images.gr-assets.com/users/1482797645p2/24311023.jpg",
+    "username": "Sara",
+    "rate": "3",
+    "review": `There’s a lot of information in this book. Describes in detail the books Lincoln did and may have read. As well as exactly what the books encompassed. Not even halfway through I started skimming. Some interesting facts. I learned a lot about Abraham Lincoln from this book.`,
+  },
+  {
+    "id": 38,
+    "profilePicture":"https://images.gr-assets.com/users/1661891365p2/35905360.jpg",
+    "username": "Ric",
+    "rate": "5",
+    "review": `Randall Munroe is one of my favorite humans of all time, so hearing he was writing a sequel to What If made my year. What If 2 has the same quality as the original; some truly ridiculous questions and even crazier answers. However, one difference was that there were a lot more questions answered and because of that the answers were shorter. It wasn’t necessarily a bad thing, just slightly different in feel than the first one.`,
+  },
+  {
+    "id": 39,
+    "profilePicture":"https://images.gr-assets.com/users/1442615218p2/811989.jpg",
+    "username": "Douglas Summers-Stay ",
+    "rate": "5",
+    "review": `I’m a fan. I’m SUCH a fan. Munroe’s books are the science books you wished you were taught from, and Munroe is the science teacher you wish you had. The man’s qualified too, he’s a former NASA physicist who decided to cartoon his way to pop science instead. And good for us, readers, because his books are not only very educational but are wildly entertaining to boot.
+    Picking up where the first What If left off, this book presents more real science answers to unreal and implausible scenarios.`,
+  },
+  {
+    "id": 40,
+    "profilePicture":"https://images.gr-assets.com/users/1659908427p2/63294595.jpg",
+    "username": "Florian",
+    "rate": "3",
+    "review": `Both the questions and the answers seemed a little less exciting than in the first volume, still highly entertaining book.`,
+  },
+];
